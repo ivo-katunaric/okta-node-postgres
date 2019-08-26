9a3aecb4467519c8cc484b73fbc2c9c4b4f67589
Ivo Katunaric <ikatun@gmail.com>
2019-08-26 14:41:05 +0200

    Make the code more linear to read

--- src/auth.module/auth-middleware.ts
+++ src/auth.module/auth-middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 import { getSessionBySessionId } from './okta-client';
 import { assertUser } from '../user.module/assert-user';
 
-export class AuthMiddleware implements NestMiddleware {
+export default class AuthMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: Function) {
     const { sessionId } = req.cookies;
     if (!sessionId) {
